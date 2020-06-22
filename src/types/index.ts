8380e28a521d0d79bbc76a1176874761fee5f4af
xuhaiqiang <xuhaiqiang@xiaoheiban.cn>
2020-06-22 22:41:50 +0800

    配置化

--- src/types/index.ts
+++ src/types/index.ts
@@ -22,11 +22,13 @@ export interface AxiosRequestConfig {
   headers?: any
   responseType?: XMLHttpRequestResponseType
   timeout?: number
+  transformRequest?: AxiosTransformer | AxiosTransformer[]
+  transformResponse?: AxiosTransformer | AxiosTransformer[]
 
   [propName: string]: any
 }
 
-export interface AxiosResponse<T=any> {
+export interface AxiosResponse<T = any> {
   data: T
   status: number
   statusText: string
@@ -35,9 +37,7 @@ export interface AxiosResponse<T=any> {
   request: any
 }
 
-export interface AxiosPromise<T=any> extends  Promise<AxiosResponse<T>> {
-
-}
+export interface AxiosPromise<T = any> extends Promise<AxiosResponse<T>> {}
 
 export interface AxiosError extends Error {
   config: AxiosRequestConfig
@@ -51,24 +51,28 @@ export interface Axios {
   defaults: AxiosRequestConfig
   interceptors: {
     request: AxiosInterceptorManager<AxiosRequestConfig>
-    response:AxiosInterceptorManager<AxiosResponse>
+    response: AxiosInterceptorManager<AxiosResponse>
   }
-  request<T=any>(config: AxiosRequestConfig): AxiosPromise<T>
+  request<T = any>(config: AxiosRequestConfig): AxiosPromise<T>
 
-  get<T=any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
-  delete<T=any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
-  head<T=any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
-  options<T=any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
+  get<T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
+  delete<T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
+  head<T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
+  options<T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
 
-  post<T=any>(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise<T>
-  put<T=any>(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise<T>
-  patch<T=any>(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise<T>
+  post<T = any>(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise<T>
+  put<T = any>(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise<T>
+  patch<T = any>(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise<T>
 }
 
-export interface AxiosInstance extends Axios{
-  <T=any>(config: AxiosRequestConfig): AxiosPromise<T>
+export interface AxiosInstance extends Axios {
+  <T = any>(config: AxiosRequestConfig): AxiosPromise<T>
 
-  <T=any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
+  <T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
+}
+
+export interface AxiosStatic extends AxiosInstance {
+  create(config?: AxiosRequestConfig): AxiosInstance
 }
 
 export interface AxiosInterceptorManager<T> {
@@ -77,9 +81,13 @@ export interface AxiosInterceptorManager<T> {
 }
 
 export interface ResolvedFn<T> {
-  (val: T):T|Promise<T>
+  (val: T): T | Promise<T>
 }
 
 export interface RejectedFn {
   (error: any): any
+}
+
+export interface AxiosTransformer {
+  (data: any, headers?: any): any
 }
