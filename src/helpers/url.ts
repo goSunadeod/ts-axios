f73d86b1fb3c26b698defc07066b5d1041ecf980
xuhaiqiang <xuhaiqiang@xiaoheiban.cn>
2020-06-30 23:17:53 +0800

    more

--- src/helpers/url.ts
+++ src/helpers/url.ts
@@ -1,24 +1,27 @@
-import {isDate, isPlainObject} from './util'
+import { isDate, isPlainObject } from './util'
+interface URLOrigin {
+  protocol: string
+  host: string
+}
 
 function encode(val: string): string {
   return encodeURIComponent(val)
     .replace(/%40/g, '@')
-    .replace(/%3A/ig, ':')
+    .replace(/%3A/gi, ':')
     .replace(/%24/g, '$')
-    .replace(/%2C/ig, ',')
+    .replace(/%2C/gi, ',')
     .replace(/%20/g, '+') // 空格变+号
-    .replace(/%5B/ig, '[')
-    .replace(/%5D/ig, ']')
-
+    .replace(/%5B/gi, '[')
+    .replace(/%5D/gi, ']')
 }
 
 export function buildURL(url: string, params?: any): string {
-  if(!params) {
+  if (!params) {
     return url
   }
   const parts: string[] = []
 
-  Object.keys(params).forEach((key) => {
+  Object.keys(params).forEach(key => {
     const val = params[key]
     if (val === null || typeof val === 'undefined') {
       return
@@ -30,11 +33,11 @@ export function buildURL(url: string, params?: any): string {
     } else {
       values = [val]
     }
-    values.forEach((val) => {
+    values.forEach(val => {
       if (isDate(val)) {
         val = val.toISOString()
-      } else if(isPlainObject(val)) {
-        val  = JSON.stringify(val)
+      } else if (isPlainObject(val)) {
+        val = JSON.stringify(val)
       }
       parts.push(`${encode(key)}=${encode(val)}`)
     })
@@ -46,7 +49,27 @@ export function buildURL(url: string, params?: any): string {
     if (markIndex !== -1) {
       url = url.slice(0, markIndex)
     }
-   url += (url.indexOf('?') === -1 ? '?' : '&') + serializedParams
+    url += (url.indexOf('?') === -1 ? '?' : '&') + serializedParams
   }
   return url
+}
+
+export function isURLSameOrigin(requestURL: string): boolean {
+  const parsedOrigin = resolveURL(requestURL)
+  return (
+    parsedOrigin.protocol === currentOrigin.protocol && parsedOrigin.host === currentOrigin.host
+  )
+}
+
+const urlParsingNode = document.createElement('a')
+const currentOrigin = resolveURL(window.location.href)
+
+function resolveURL(url: string): URLOrigin {
+  urlParsingNode.setAttribute('href', url)
+  const { protocol, host } = urlParsingNode
+
+  return {
+    protocol,
+    host
+  }
 }
